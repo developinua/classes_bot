5becd256292405d44011ee0483df69acc992b9cd
Nazar Bulyha <nbulyha@fieldcomplete.com>
2023-12-05 03:06:04 +0200

    Refactor checkin handler to be more readable.

--- Classes.Application/Handlers/Checkin/CheckinCallbackHandler.cs
+++ Classes.Application/Handlers/Checkin/CheckinCallbackHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Classes.Application.Services;
 using Classes.Domain.Requests;
@@ -11,7 +12,6 @@ namespace Classes.Application.Handlers.Checkin;
 
 public class CheckinCallbackHandler(
         IBotService botService,
-        ICallbackExtractorService callbackExtractorService,
         IUserSubscriptionService userSubscriptionService,
         IValidator<CallbackQuery> callbackQueryValidator)
     : IRequestHandler<CheckinCallbackRequest, Result>
@@ -23,41 +23,45 @@ public class CheckinCallbackHandler(
 
         await botService.SendChatActionAsync(request.ChatId, cancellationToken);
 
-        var userSubscriptionId = callbackExtractorService.GetUserSubscriptionIdFromCallback(
-            request.CallbackQuery.Data!,
-            request.CallbackPattern);
-        var userSubscription = await userSubscriptionService.GetById(userSubscriptionId);
+        var userSubscription = await userSubscriptionService.GetFromCallback(
+            request.CallbackQuery, request.CallbackPattern);
 
         if (userSubscription.Data is null)
-        {
-            await botService.SendTextMessageAsync(
-                request.ChatId,
-                "You haven't that subscription. Please contact @nazikBro for details.",
-                cancellationToken);
-            return Result.Failure().WithMessage("User subscription not found.");
-        }
-
-        if (userSubscription.Data.RemainingClasses == 0)
-        {
-            await botService.SendTextMessageAsync(
-                request.ChatId,
-                "You haven't any available classes. Press /subscriptions to manage your subscriptions.",
-                cancellationToken);
-            return Result.Failure().WithMessage("No remaining classes left.");
-        }
+            return await HandleSubscriptionNotFoundFailure();
         
         var checkinResult = await userSubscriptionService.CheckinOnClass(userSubscription.Data);
 
         if (checkinResult.IsFailure())
+            return await HandleCheckinFailure();
+        
+        await botService.SendTextMessageAsync(request.ChatId, "*💚*", cancellationToken);
+        return Result.Success();
+        
+        async Task<Result> HandleSubscriptionNotFoundFailure()
         {
+            await botService.SendTextMessageAsync(
+                request.ChatId,
+                "You haven't this subscription. Please contact @nazikBro for details.",
+                cancellationToken);
+            return Result.Failure().WithMessage("User subscription not found.");
+        }
+        
+        async Task<Result> HandleCheckinFailure()
+        {
+            if (checkinResult.Errors.Any())
+            {
+                await botService.SendTextMessageAsync(
+                    request.ChatId,
+                    checkinResult.Errors.First().Message,
+                    cancellationToken);
+                return Result.Failure().WithMessage(checkinResult.Errors.First().Message);
+            }
+
             await botService.SendTextMessageAsync(
                 request.ChatId,
                 "There was a problem with class check-in. Please contact @nazikBro for details.",
                 cancellationToken);
             return Result.Failure().WithMessage("There was a problem with class check-in.");
         }
-        
-        await botService.SendTextMessageAsync(request.ChatId, "*💚*", cancellationToken);
-        return Result.Success();
     }
 }
--- Classes.Application/Services/UserSubscriptionService.cs
+++ Classes.Application/Services/UserSubscriptionService.cs
@@ -1,27 +1,38 @@
+using System.Threading;
 using System.Threading.Tasks;
 using Classes.Data.Repositories;
 using Classes.Domain.Models;
 using Microsoft.Extensions.Logging;
 using ResultNet;
+using Telegram.Bot.Types;
 
 namespace Classes.Application.Services;
 
 public interface IUserSubscriptionService
 {
-    Task<Result<UserSubscription?>> GetById(long id);
+    Task<Result<UserSubscription?>> GetFromCallback(CallbackQuery callback, string callbackPattern);
     Task<Result> CheckinOnClass(UserSubscription userSubscription);
 }
 
 public class UserSubscriptionService(
+        ICallbackExtractorService callbackExtractorService,
         IUserSubscriptionRepository userSubscriptionRepository,
         ILogger<UserService> logger)
     : IUserSubscriptionService
 {
-    public async Task<Result<UserSubscription?>> GetById(long id) =>
-        await userSubscriptionRepository.GetById(id);
+    public async Task<Result<UserSubscription?>> GetFromCallback(CallbackQuery callback, string callbackPattern)
+    {
+        var userSubscriptionId = callbackExtractorService.GetUserSubscriptionIdFromCallback(
+            callback.Data!, callbackPattern);
+        return await userSubscriptionRepository.GetById(userSubscriptionId);
+    }
 
     public async Task<Result> CheckinOnClass(UserSubscription userSubscription)
     {
+        if (userSubscription.RemainingClasses == 0)
+            return Result.Failure()
+                .WithMessage("You haven't any available classes. Press /subscriptions to manage your subscriptions.");
+        
         userSubscription.RemainingClasses--;
         return await userSubscriptionRepository.Update(userSubscription);
     }
